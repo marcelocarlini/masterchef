e57bd59fbf0f4b42419979c4dac2cbb45417bb8e
Marcelo <marcelocarlini@hotmail.com>
2023-10-26 18:31:19 -0300

    Testes script gpt_7

--- src/components/Home.js
+++ src/components/Home.js
@@ -23,7 +23,7 @@ const Home = () => {
         setIsProcessing(true)
         //function here
         const response = await openai.createCompletion({
-            model: "text-davinci-003",
+            model: "text-ada-001",
             prompt: `Apresente uma receita bem elaborada e existente de restaurante gourmet feita com ${ingredientes}, dentro de uma div com classe ingredientes crie um cabeçalho principal com o nome da receita e outro cabeçalho html com a palavra ingredientes e em seguida
             liste os ingredientes em uma html ul li, faça o mesmo com o modo de preparo.`,
             temperature: 0.9,
